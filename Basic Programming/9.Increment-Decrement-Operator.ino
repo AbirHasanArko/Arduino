218557054d498fd732f932d527aba37cb07278df
Abir Hasan Arko <140732716+arko-hasan@users.noreply.github.com>
2023-07-29 23:56:44 +0600

    Create 9.Increment-Decrement-Operator.ino
    This Arduino Program shows how to do Increment and Decrement operation.

---
+++ Basic Programming/9.Increment-Decrement-Operator.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  Serial.begin(9600);
+  int i = 5;
+  Serial.println(i);
+  i = i+5;
+  Serial.println(i);
+  i++;
+  Serial.println(i);
+  i--;
+  Serial.println(i);
+}
+
+void loop()
+{
+
+}
